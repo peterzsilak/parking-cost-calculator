6162fa905068a9abc93270f47050fe3d5b95a9b1
peterzsilak <peter.zsilak@gmail.com>
2019-10-21 14:51:57 +0200

    Started to create a Cucumber-Java-Selenium project

---
+++ src/test/java/com/liferay/uiautomation/stepDefinitions/ParkingSteps.java
@@ -0,0 +1,38 @@
+package com.liferay.uiautomation.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ParkingSteps {
+    @Given("the parking cost calculator system")
+    public void the_parking_cost_calculator_system() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("the user selects the Valet rate")
+    public void the_user_selects_the_Valet_rate() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("the user starts the parking at {string} and {string} and sets {string}")
+    public void the_user_starts_the_parking_at_and_and_sets(String string, String string2, String string3) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("the user closes the parking at {string} and {string} and sets {string}")
+    public void the_user_closes_the_parking_at_and_and_sets(String string, String string2, String string3) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the system estimates the {string} ticket price")
+    public void the_system_estimates_the_ticket_price(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+}
